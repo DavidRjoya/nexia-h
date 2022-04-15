b8d392870c612a11aea301e9aac362593ff9a429
David <dralvarado@ucompensar.edu.co>
2022-04-15 01:12:52 -0500

    Cambios David
    Cambio todo
    INCLUDE DE MENU
    SE AGREGO RESERVAS
    SE CORRIGIERON ERRORES

---
+++ js/reservas.js
@@ -0,0 +1,17 @@
+/* **************alerta************* */
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="submit" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+    alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', function() {
+        alert('Excelente tilin, Reservaste con nosotros!', 'success')
+    })
+}
